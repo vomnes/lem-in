8bc5d54f83b47b68b7fcc6c11f355823543bbd87
Valentin OMNES <vomnes@student.42.fr>
2017-03-08 12:49:08 +0100

    Update main.c all

--- src/main.c
+++ src/main.c
@@ -6,13 +6,45 @@
 /*   By: vomnes <vomnes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/08 10:43:28 by vomnes            #+#    #+#             */
-/*   Updated: 2017/03/08 10:43:49 by vomnes           ###   ########.fr       */
+/*   Updated: 2017/03/08 12:43:49 by vomnes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int main(void)
+int ft_get_input(int fd)
 {
-  return (0);
+	char *line;
+
+	line = NULL;
+	while (get_next_line(fd, &line) > 0)
+	{
+		ft_putendl(line);
+		ft_strdel(&line);
+	}
+	ft_strdel(&line);
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	int fd;
+
+	if (argc <= 0)
+		fd = 0;
+	else if (argc == 1 || argc == 2)
+	{
+		if ((fd = open(argv[1], O_RDONLY)) < 1)
+		{
+			ft_putendl_fd("Error - Failed to open file", 2);
+			return (-1);
+		}
+		ft_get_input(fd);
+	}
+	else
+	{
+		ft_putendl("Usage : ./lem-in [file_name]");
+		return (0);
+	}
+	return (0);
 }
