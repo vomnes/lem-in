f83504fef97593152a5c4967aa65e018b43dc900
Valentin OMNES <vomnes@student.42.fr>
2017-03-29 16:34:43 +0200

    Add path_len

---
+++ src/path_len.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path_len.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vomnes <vomnes@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/29 16:31:57 by vomnes            #+#    #+#             */
+/*   Updated: 2017/03/29 16:31:58 by vomnes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/lem_in.h"
+
+float	path_len(t_path *list)
+{
+	t_path	*temp;
+	float	count;
+
+	temp = list;
+	count = 0;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
